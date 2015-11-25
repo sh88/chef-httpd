491a87bbf8a62aab0144c76756358d5ecea64a14
tkuchiki <kuchiki.taku@gmail.com>
2015-11-26 01:05:01 +0900

    Support httpd 2.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,21 @@
 default[:httpd] = {
-  :server_root        => "/etc/httpd",
-  :timeout            => 60,
-  :keepalive          => "Off",
-  :keepalive_requests => 100,
-  :keepalive_timeout  => 15,
+  :ius_base_uri => "https://%{platform}%{platform_version}.iuscommunity.org/ius-release.rpm",
+  :use_ius      => false,
+  :use_scl      => false,
+  :version      => 2.2,
+  :owner        => "apache",
+  :group        => "apache",
+  :sysconfig    => "/etc/sysconfig/httpd",
+  :httpd_conf   => "/etc/httpd/conf/httpd.conf",
+  :packages     => [],
+  :nss_sdb_use_cache         => "YES",
+  :server_root               => "/etc/httpd",
+  :timeout                   => 60,
+  :keepalive                 => "Off",
+  :keepalive_requests        => 100,
+  :keepalive_timeout         => 15,
+  :max_request_workers       => 50,
+  :max_connections_per_child => 1000,
   :prefork => {
       :start_servers          => 8,
       :min_spare_servers      => 75,
@@ -31,3 +43,19 @@ default[:httpd] = {
   ]
 }
 
+if node[:httpd][:use_scl]
+  default[:httpd][:version] = 2.4 
+  default[:httpd][:version_without_dot] = "24"
+  default[:httpd][:sysconfig]  = "/opt/rh/httpd24/root/etc/sysconfig/httpd" 
+  default[:httpd][:httpd_conf] = "/opt/rh/httpd24/root/etc/httpd/conf/httpd.conf"
+elsif node[:httpd][:use_ius]
+  default[:httpd][:version] = 2.4 
+  default[:httpd][:version_without_dot] = "24"
+else
+  default[:httpd][:version_without_dot] = node[:httpd][:version].to_s.delete('.')
+end
+
+default[:httpd][:ius_rpm_uri] = node[:httpd][:ius_base_uri] % {
+  :platform         => node[:platform],
+  :platform_version => node[:platform_version].to_i,
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,20 +1,87 @@
-package "httpd"
+case node[:platform]
+when "centos", "rhel"
+  if node[:httpd][:use_scl]
+    package "centos-release-SCL" do
+      action :install
+    end
 
-template "/etc/httpd/conf/httpd.conf" do
+    server_package = "httpd24-httpd"
+    devel_package  = "httpd24-httpd-devel"
+    service_name   = "httpd24-httpd"
+  elsif node[:httpd][:use_ius]
+    package "epel-release" do
+      action :install
+    end
+    
+    ius_rpm = "#{Chef::Config[:file_cache_path]}/#{File.basename(node[:httpd][:ius_rpm_uri])}"
+
+    remote_file "#{ius_rpm}" do
+      source node[:httpd][:ius_rpm_uri]
+      not_if "rpm -q ius-release"
+    end
+
+    rpm_package ius_rpm do
+      action :install
+      not_if "rpm -q ius-release"
+    end
+
+    file ius_rpm do
+      action :delete
+    end
+
+    server_package = "httpd24u"
+    devel_package  = "httpd24u-devel"
+    service_name   = "httpd"
+  else
+    server_package = "httpd"
+    devel_package  = "httpd-devel"
+  end
+end
+
+package server_package do
+  action :install
+end
+
+node[:httpd][:packages].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+file node[:httpd][:sysconfig] do
+  content lazy {
+    f = Chef::Util::FileEdit.new(path)
+    f.insert_line_if_no_match(/NSS_SDB_USE_CACHE/, "export NSS_SDB_USE_CACHE=#{node[:httpd][:nss_sdb_use_cache]}\n")
+    f.send(:editor).lines.join
+  }
+end
+
+template node[:httpd][:httpd_conf] do
   owner    "root"
   mode     0644
-  source   "httpd.conf.erb"
+  source   "httpd#{node[:httpd][:version_without_dot]}.conf.erb"
   supports :status => true, :restart => true, :reload => true
-  notifies :reload, "service[httpd]"
+  notifies :reload, "service[#{service_name}]"
+end
+
+link node[:httpd][:server_root] do
+  to lazy { "/opt/rh/httpd24/root/etc/httpd" }
+  only_if { node[:httpd][:use_scl] }
+end
+
+link "/usr/lib64/httpd" do
+  to lazy { "/opt/rh/httpd24/root/usr/lib64/httpd" }
+  only_if { node[:httpd][:use_scl] }
 end
 
 directory node[:httpd][:document_root] do
-  owner "apache"
-  group "apache"
-  mode  0755
+  owner     node[:httpd][:owner]
+  group     node[:httpd][:group]
+  mode      0755
+  recursive true
 end
 
-service "httpd" do
+service service_name do
   action [:enable, :start]
-  reload_command "service httpd graceful"
+  reload_command "service #{service_name} graceful"
 end
